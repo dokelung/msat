b70e8586be4818404a0e871d2e6eeb3f21824169
dokelung <dokelung@gmail.com>
2015-06-03 00:52:19 +0800

    add setup files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+    name = 'msat',
+    packages = ['msat'],
+    scripts = ['ms'],
+    install_requires=[],
+    version = '1.0',
+    description = 'Multiset Constraint Solver for Multi-SAT',
+    author = 'dokelung',
+    author_email = 'dokelung@gmail.com',
+    url = 'https://github.com/dokelung/msat',
+    download_url = 'https://github.com/dokelung/msat/tarball/v1.0',
+    keywords = ['multiset', 'constraint', 'math'],
+    classifiers = [],
+)
